00644c56ad0c581e5395ff50a6522781d091ded7
Wch/wch <1492271677@qq.com/1492271677@qq.com>
2025-01-08 16:23:43 +0800

    1111

--- exercises/19_class_virtual_destruct/main.cpp
+++ exercises/19_class_virtual_destruct/main.cpp
@@ -1,7 +1,6 @@
 #include "../exercise.h"
 
 struct A {
-    // 静态成员变量初始化
     static int num_a;
 
     A() {
@@ -17,11 +16,9 @@ struct A {
     }
 };
 
-// 静态成员变量初始化
 int A::num_a = 0;
 
 struct B final : public A {
-    // 静态成员变量初始化
     static int num_b;
 
     B() {
@@ -32,39 +29,41 @@ struct B final : public A {
         --num_b;
     }
 
-    char name() const final {
+    char name() const override {
         return 'B';
     }
 };
 
-// 静态成员变量初始化
 int B::num_b = 0;
 
 int main(int argc, char **argv) {
-    auto a = new A;
-    auto b = new B;
-    ASSERT(A::num_a == 1, "Fill in the correct value for A::num_a");
-    ASSERT(B::num_b == 1, "Fill in the correct value for B::num_b");
-    ASSERT(a->name() == 'A', "Fill in the correct value for a->name()");
-    ASSERT(b->name() == 'B', "Fill in the correct value for b->name()");
+    {
+        auto a = new A;
+        auto b = new B;
+        ASSERT(A::num_a == 1, "A::num_a should be 1 after creating one A object");
+        ASSERT(B::num_b == 1, "B::num_b should be 1 after creating one B object");
+        ASSERT(a->name() == 'A', "a->name() should return 'A'");
+        ASSERT(b->name() == 'B', "b->name() should return 'B'");
 
-    delete a;
-    delete b;
-    ASSERT(A::num_a == 0, "Every A was destroyed");
-    ASSERT(B::num_b == 0, "Every B was destroyed");
+        delete a;
+        delete b;
+        ASSERT(A::num_a == 0, "A::num_a should be 0 after deleting the A object");
+        ASSERT(B::num_b == 0, "B::num_b should be 0 after deleting the B object");
+    }
 
-    A *ab = new B; // 派生类指针可以随意转换为基类指针
-    ASSERT(A::num_a == 1, "Fill in the correct value for A::num_a");
-    ASSERT(B::num_b == 1, "Fill in the correct value for B::num_b");
-    ASSERT(ab->name() == 'B', "Fill in the correct value for ab->name()");
+    {
+        A *ab = new B;
+        ASSERT(A::num_a == 1, "A::num_a should be 1 after creating one B object via A*");
+        ASSERT(B::num_b == 1, "B::num_b should be 1 after creating one B object via A*");
+        ASSERT(ab->name() == 'B', "ab->name() should return 'B' when B is actually created");
 
-    // 正确的类型转换语句
-    B &bb = dynamic_cast<B&>(*ab); // 使用 dynamic_cast 转换基类指针为派生类引用
-    ASSERT(bb.name() == 'B', "Fill in the correct value for bb->name()");
+        B &bb = dynamic_cast<B&>(*ab);
+        ASSERT(bb.name() == 'B', "bb.name() should return 'B'");
 
-    delete ab; // 通过指针可以删除指向的对象，即使是多态对象
-    ASSERT(A::num_a == 0, "Every A was destroyed");
-    ASSERT(B::num_b == 0, "Every B was destroyed");
+        delete ab;
+        ASSERT(A::num_a == 0, "A::num_a should be 0 after deleting the B object via A*");
+        ASSERT(B::num_b == 0, "B::num_b should be 0 after deleting the B object via A*");
+    }
 
     return 0;
 }
